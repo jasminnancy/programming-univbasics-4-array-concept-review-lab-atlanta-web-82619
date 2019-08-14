6e27aa25d93d50ff82a944c12b7b4101473ae596
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-14 23:36:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,8 @@ def find_element_index(array, value_to_find)
   while count < length do
     if array[count] = value_to_find
       puts array.index[count]
-    else puts
+    else puts nil
+    count += 1
   end  
 end
 
