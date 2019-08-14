df8e9a13a2459b4215551643d998c9dbefc7d1f7
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-14 23:42:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   
   while control < array.length do
     if array[control] == value_to_find
-      puts array[control].index
+      puts array.index(control)
     else
       puts nil
     end
