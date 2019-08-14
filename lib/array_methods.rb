d2f2b3fc30a9450dd2c44569e24ef74c04e7b773
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-14 23:23:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,6 @@
 def find_element_index(array, value_to_find)
-  control = 0
+  length = array.length
   
-  while control < array.length {
-    if array[control] === value_to_find {
-      return value_to_find.index[control]
-    }
-    else control += 1
-  }
 end
 
 def find_max_value(array)
